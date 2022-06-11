f092b7130bfcb3733c079a67bada6348b1dc00a1
Avery Yang <averyyang2674@gmail.com>
2022-06-10 21:49:58 -0500

    change order of js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$(() => {
-  readyNow
-});
-
 const open = $('#open')
 const close = $('#close')
 const container = $('.container')
+
+$(() => {
+  readyNow
+});
 
 function readyNow() {
   open.on('click', () => {
